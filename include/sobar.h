ff411e1e8fcbfa1f860a3685c341a33b6a140e6f
Takashi Kawasaki <espresso3389@gmail.com>
2021-12-21 17:26:12 +0900

    Add sbr_PdfText* functions.

--- include/sobar.h
+++ include/sobar.h
@@ -36,21 +36,22 @@ extern "C"
 	typedef struct sbr_PdfDocumentStruct *sbr_PdfDocument;
 	typedef struct sbr_PdfPageStruct *sbr_PdfPage;
 	typedef struct sbr_PdfBitmapStruct *sbr_PdfBitmap;
+	typedef struct sbr_PdfTextStruct *sbr_PdfText;
 #endif
 
 	sbr_EXPORT void sbr_API sbr_Initialize();
 	sbr_EXPORT void sbr_API sbr_Finalize();
 
 	sbr_EXPORT sbr_PdfDocument sbr_API sbr_PdfDocumentOpenFile(
-			const char *utf8FileName, const char *utf8Password);
+		const char *utf8FileName, const char *utf8Password);
 	sbr_EXPORT sbr_PdfDocument sbr_API sbr_PdfDocumentOpenMemory(
-			const void *data, unsigned int size, const char *utf8Password);
+		const void *data, unsigned int size, const char *utf8Password);
 
 	typedef unsigned int (*sbr_ContextReadCallback)(void *context, void *buffer, unsigned int offset, unsigned int length);
 	typedef void (*sbr_ContextReleaseCallback)(void *context);
 
 	sbr_EXPORT sbr_PdfDocument sbr_API sbr_PdfDocumentOpenCustom(
-			void *context, unsigned int size, sbr_ContextReadCallback read, sbr_ContextReleaseCallback release, const char *utf8Password);
+		void *context, unsigned int size, sbr_ContextReadCallback read, sbr_ContextReleaseCallback release, const char *utf8Password);
 	sbr_EXPORT void sbr_API sbr_PdfDocumentClose(sbr_PdfDocument doc);
 	sbr_EXPORT int sbr_API sbr_PdfDocumentGetPageCount(sbr_PdfDocument doc);
 	sbr_EXPORT sbr_PdfPage sbr_API sbr_PdfDocumentLoadPage(sbr_PdfDocument doc, int pageIndex);
@@ -115,15 +116,15 @@ extern "C"
 	};
 
 	sbr_EXPORT int sbr_API sbr_PdfPageRender(
-			sbr_PdfPage page,
-			sbr_PdfBitmap bmp, int x, int y,
-			int width, int height,
-			sbr_RotateClockwise rotate,
-			int flags);
+		sbr_PdfPage page,
+		sbr_PdfBitmap bmp, int x, int y,
+		int width, int height,
+		sbr_RotateClockwise rotate,
+		int flags);
 
 	sbr_EXPORT sbr_PdfBitmap sbr_API sbr_PdfBitmapCreate(
-			int width, int height, sbr_PixelFormat format, long stride, void *scan0,
-			sbr_PdfBitmapOnReleaseCallback callback, void *context);
+		int width, int height, sbr_PixelFormat format, long stride, void *scan0,
+		sbr_PdfBitmapOnReleaseCallback callback, void *context);
 	sbr_EXPORT void sbr_API sbr_PdfBitmapRelease(sbr_PdfBitmap bmp);
 	sbr_EXPORT sbr_PixelFormat sbr_API sbr_PdfBitmapGetPixelFormat(sbr_PdfBitmap bmp);
 	sbr_EXPORT unsigned char *sbr_API sbr_PdfBitmapGetScan0Pointer(sbr_PdfBitmap bmp);
@@ -131,6 +132,53 @@ extern "C"
 	sbr_EXPORT int sbr_API sbr_PdfBitmapGetWidth(sbr_PdfBitmap bmp);
 	sbr_EXPORT int sbr_API sbr_PdfBitmapGetHeight(sbr_PdfBitmap bmp);
 
+	typedef struct sbr_PdfRectStruct
+	{
+		float left;
+		float bottom;
+		float right;
+		float top;
+	} sbr_PdfRect;
+
+	typedef enum sbr_PdfCharTypeEnum
+	{
+		sbr_pctError = -1,
+		sbr_pctNormal = 0,
+		sbr_pctGenerated = 1,
+		sbr_pctUnUnicode = 2,
+		sbr_pctHyphen = 3,
+		sbr_pctPiece = 4
+	} sbr_PdfCharType;
+
+	typedef struct sbr_PdfCharInfoStruct
+	{
+		unsigned int unicode;		 // Unicode codepoint
+		unsigned int nativeCharCode; // Native char code
+		float emSize;
+		float originX, originY;
+		sbr_PdfRect charBox;
+		sbr_PdfCharType charType; // sbr_pct*
+	} sbr_PdfCharInfo;
+
+	sbr_EXPORT void sbr_API sbr_PdfStringRelease(const char *str);
+
+	sbr_EXPORT sbr_PdfText sbr_API sbr_PdfTextLoadFromPage(sbr_PdfPage page);
+	sbr_EXPORT int sbr_API sbr_PdfTextClose(sbr_PdfText text);
+	sbr_EXPORT int sbr_API sbr_PdfTextGetLength(sbr_PdfText text);
+	sbr_EXPORT int sbr_API sbr_PdfTextGetCharInfoAt(sbr_PdfText text, int index, sbr_PdfCharInfo *info);
+	sbr_EXPORT int sbr_API sbr_PdfTextGetIndexForPoint(sbr_PdfText text, float x, float y, float xTolerance, float yTolerance);
+	sbr_EXPORT int sbr_API sbr_PdfTextGetRectCount(sbr_PdfText text, int index, int count);
+	// rects: at least equal or larger than the count obtained by sbr_PdfTextGetRectCount
+	sbr_EXPORT int sbr_API sbr_PdfTextGetRects(sbr_PdfText text, int index, int count, sbr_PdfRect *rects);
+	// The returned string should be release with sbr_PdfStringRelease
+	sbr_EXPORT char *sbr_API sbr_PdfTextGetText(sbr_PdfText text, int index, int count);
+
+	typedef int (*sbr_PdfCharCallback)(void *context, int index, const sbr_PdfCharInfo *charInfo);
+
+	sbr_EXPORT int sbr_PdfTextEnumerateCharsInRect(sbr_PdfText text, sbr_PdfRect *rect, void *context, sbr_PdfCharCallback callback);
+
+	sbr_EXPORT int sbr_API sbr_PdfTextGetRectsByRect(sbr_PdfText text, const sbr_PdfRect *rect, sbr_PdfRect *buffer, int count);
+	sbr_EXPORT char *sbr_API sbr_PdfTextGetTextByRect(sbr_PdfText text, const sbr_PdfRect *rect);
 #ifdef __cplusplus
 }
 #endif
