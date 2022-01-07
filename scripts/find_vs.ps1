5bc0529bc08094c22113d29d352c88f1ae68f922
Takashi Kawasaki <espresso3389@gmail.com>
2022-01-07 22:32:47 +0900

    Update find_vs.ps1 to use Enter-VsDevShell.

--- scripts/find_vs.ps1
+++ scripts/find_vs.ps1
@@ -23,28 +23,20 @@ Write-Host "vswhere found at: $vsWhere" -ForegroundColor Yellow
 #
 # Get path to Visual Studio installation using vswhere.
 #
-$vsPath = &$vsWhere -utf8 -latest -products * -requires Microsoft.Component.MSBuild -version "[16.0,17.0)" -property installationPath
+$vsPath = &$vsWhere -utf8 -latest -property installationPath
 If ([string]::IsNullOrEmpty("$vsPath")) {
   Write-Error "Failed to find Visual Studio installation. Aborting." -ErrorAction Stop
 }
 Write-Host "Using Visual Studio installation at: ${vsPath}" -ForegroundColor Yellow
 
+#
+# Load Microsoft.VisualStudio.DevShell.dll for Enter-VsDevShell
+#
+Import-Module (Join-Path $vsPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
 
 #
-# Make sure the Visual Studio Command Prompt variables are set.
 #
-If (Test-Path env:LIBPATH) {
-  Write-Host "Visual Studio Command Prompt variables already set." -ForegroundColor Yellow
-}
-Else {
-  # Load VC vars
-  Push-Location "${vsPath}\VC\Auxiliary\Build"
-  cmd /c "vcvarsall.bat $Arch&set" |
-  ForEach-Object {
-    If ($_ -match "=") {
-      $v = $_.split("="); Set-Item -Force -Path "ENV:\$($v[0])" -Value "$($v[1])"
-    }
-  }
-  Pop-Location
-  Write-Host "Visual Studio Command Prompt variables for $Arch set." -ForegroundColor Yellow
-}
+#
+Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation -DevCmdArguments "-arch=$Arch"
+Write-Host "Visual Studio Command Prompt variables for $Arch set." -ForegroundColor Yellow
+
